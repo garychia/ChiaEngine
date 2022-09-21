af73e8fc11221453e88ea8cf89672f8ad635ea03
Chia-Hsiang Cheng <s940046@hotmail.com>
2022-09-21 16:41:46 +0800

    Remove macros that lead to pollution

--- src/Data/String.hpp
+++ src/Data/String.hpp
@@ -1,29 +1,26 @@
 #ifndef STRING_HPP
 #define STRING_HPP
 
-#define STR(x) String((const wchar_t *)(L##x), (size_t)(sizeof(L##x) / sizeof(wchar_t) - 1))
-#define CHAR char16_t
-
 #include "Types/Types.hpp"
 #include "Str.hpp"
 
-class String : public Str<CHAR>
+class String : public Str<char16_t>
 {
 public:
-    String() noexcept : Str<CHAR>() {}
+    String() noexcept : Str<char16_t>() {}
 
-    String(const Str<CHAR> &s) noexcept : Str<CHAR>(s) {}
+    String(const Str<char16_t> &s) noexcept : Str<char16_t>(s) {}
 
-    String(Str<CHAR> &&s) noexcept : Str<CHAR>(Move(s)) {}
-
-    template <class T>
-    String(const T &c, size_t length) noexcept : Str<CHAR>(c, length) {}
+    String(Str<char16_t> &&s) noexcept : Str<char16_t>(Move(s)) {}
 
     template <class T>
-    String(const T *str, size_t length) noexcept : Str<CHAR>(str, length) {}
+    String(const T &c, size_t length) noexcept : Str<char16_t>(c, length) {}
+
+    template <class T>
+    String(const T *str, size_t length) noexcept : Str<char16_t>(str, length) {}
 };
 
-class StringStream : StrStream<String::CharType>
+class StringStream : public StrStream<String::CharType>
 {
 public:
     StringStream() : StrStream<String::CharType>() {}
