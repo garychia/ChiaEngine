7088934478b8628b280c5d8c86bd1a89443b4eff
Chia-Hsiang Cheng <s940046@hotmail.com>
2022-10-20 12:28:21 +0800

    Implement interactable GUI elements

---
+++ src/Display/GUI/IInteractable.cpp
@@ -0,0 +1,26 @@
+#include "IInteractable.hpp"
+
+bool IInteractable::WithIn(const Point2D &coordinates) const
+{
+    const auto topLeft = GetTopLeftPosition();
+    const auto width = GetWidth();
+    const auto height = GetHeight();
+    return coordinates.x >= topLeft.x && coordinates.x <= topLeft.x + width && coordinates.y >= topLeft.y &&
+           coordinates.y <= topLeft.y + height;
+}
+
+void IInteractable::OnMouseDown(const Point2D &coordinates)
+{
+}
+
+void IInteractable::OnMouseUp(const Point2D &coordinates)
+{
+}
+
+void IInteractable::OnClicked(const Point2D &coordinates)
+{
+}
+
+void IInteractable::OnHovered(const Point2D &coordinates)
+{
+}
---
+++ src/Display/GUI/IInteractable.hpp
@@ -0,0 +1,20 @@
+#ifndef IINTERACTABLE_HPP
+#define IINTERACTABLE_HPP
+
+#include "IGUI.hpp"
+
+class IInteractable : public IGUI
+{
+  public:
+    virtual bool WithIn(const Point2D &coordinates) const;
+
+    virtual void OnMouseDown(const Point2D &coordinates);
+
+    virtual void OnMouseUp(const Point2D &coordinates);
+
+    virtual void OnClicked(const Point2D &coordinates);
+    
+    virtual void OnHovered(const Point2D &coordinates);
+};
+
+#endif
